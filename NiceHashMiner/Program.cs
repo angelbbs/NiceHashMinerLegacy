afba29432444b571cf7013e5e62eb486cb96d98e
angelbbs <angelbbs@mail.ru>
2022-05-10 13:34:45 +0300

    47

--- NiceHashMiner/Program.cs
+++ NiceHashMiner/Program.cs
@@ -176,11 +176,11 @@ namespace NiceHashMiner
                     Logger.ConfigureWithFile();
                 }
 
-                if (Configs.ConfigManager.GeneralConfig.ForkFixVersion < 46.1)
+                if (Configs.ConfigManager.GeneralConfig.ForkFixVersion < 47)
                 {
                     ConfigManager.GeneralConfig.ChartEnable = false;
                     Helpers.ConsolePrint("NICEHASH", "Previous version: " + Configs.ConfigManager.GeneralConfig.ForkFixVersion.ToString());
-                    ConfigManager.GeneralConfig.ForkFixVersion = 46.1;
+                    ConfigManager.GeneralConfig.ForkFixVersion = 47;
                 }
                 //**
                 //Thread.Sleep(100);
--- NiceHashMiner/Utils/BINS_CODEGEN.cs
+++ NiceHashMiner/Utils/BINS_CODEGEN.cs
@@ -20,7 +20,7 @@ namespace NiceHashMiner.Utils
             @"/NBMiner/NBMiner.exe",
             @"/miniZ/miniZ.exe",
             @"/ethlargement/OhGodAnETHlargementPill-r2.exe",
-            @"/Fork_Fix_46.1.txt"
+            @"/Fork_Fix_47.txt"
         };
     }
 }
